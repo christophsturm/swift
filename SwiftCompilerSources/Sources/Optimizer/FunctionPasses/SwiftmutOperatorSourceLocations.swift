120017e44b8bebe52c34aa5592b18cf98f4975f4
Christoph Sturm <me@christophsturm.com>
2026-06-10 07:26:36 +0200

    Instrument generated specializations for metamutants

--- SwiftCompilerSources/Sources/Optimizer/FunctionPasses/SwiftmutOperatorSourceLocations.swift
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/SwiftmutOperatorSourceLocations.swift
@@ -22,7 +22,8 @@ func swiftmutExclusionReason(
   if swiftmutIsGeneratedInvalidLocationFunction(function) {
     return "generatedInvalidLocation"
   }
-  if swiftmutIsGeneratedSpecializationFunctionName(function.name.string) {
+  if config.mode != .metamutant,
+     swiftmutIsGeneratedSpecializationFunctionName(function.name.string) {
     return "generatedSpecialization"
   }
   let location = function.location.description
